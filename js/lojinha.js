f0c42d46426f021092991e5083639c53b2ec783c
arhur-coelho-alves <Arthur56799@gmail.com>
2026-05-26 09:08:04 -0300

    Carrinho foi feito

--- js/lojinha.js
+++ js/lojinha.js
@@ -1,27 +1,47 @@
 const produtos = [{
-  linkImg: "../assets/apple-watch-series-9.png",
-  sub: "Série 9, Caixa de Alumínio",
-  nomeP: "Apple Watch Series 9",
-  preco: 3199
-}];
-
+  linkImg: "../assets/15pmx.jpg",
+  sub: "Processador A17 Pro",
+    nomeP: "iPhone 15 Pro Max",
+    preco: 8999},
+  {
+    linkImg: "../assets/mac4pro.png",
+    sub: "Chip M3 Max",
+    nomeP: "MacBook Pro 14",
+    preco: 15499
+  },
+  {
+    linkImg: "../assets/ipadm2projpg.jpg",
+    sub: "Chip M2, Tela Liquid Retina",
+    nomeP: "iPad Pro 11",
+    preco: 6299
+  },
+  {
+    linkImg: "../assets/apple-watch-series-9.png",
+    sub: "Série 9, Caixa de Alumínio",
+    nomeP: "Apple Watch Series 9",
+    preco: 3199
+  }
+];
+let cont = 0
+let id = 0
 const sectionProdutos = document.querySelector(".grade-produtos");
 
 document.querySelector(".btn-adicionar-nav").addEventListener("click", () => {
-  produtos.forEach((produto) => {
-    const imgEach = produto.linkImg;
-    const subEach = produto.sub;
-    const nomeEach = produto.nomeP;
-    const precoEach = produto.preco;
-
-    criarProduto(imgEach, subEach, nomeEach, precoEach);
-  });
+  if (cont === 0) {
+    produtos.forEach((produto) => {
+      criarProduto(produto.linkImg, produto.sub, produto.nomeP, produto.preco);
+    });
+    cont = 1;
+  }
 });
+
 
     function criarProduto(imagem, subNome, nome, preco) {
       const articleProduto = document.createElement("article");
       articleProduto.classList.add("cartao-produto");
-
+      let idAtual = id
+      articleProduto.dataset.id = idAtual;
+      id++;
       const divImagem = document.createElement("div");
       divImagem.classList.add("produto-imagem");
       let imag = document.createElement("img")
@@ -44,7 +64,38 @@ document.querySelector(".btn-adicionar-nav").addEventListener("click", () => {
       const btnComprar = document.createElement("button");
       btnComprar.classList.add("btn-comprar");
       btnComprar.textContent = "Comprar";
+      btnComprar.addEventListener("click", ()=>{
+    comprar(idAtual)
+})
 
       articleProduto.append(divImagem, divInfo, btnComprar);
       sectionProdutos.append(articleProduto);
     }
+function comprar(idElemento){
+    let elementoAtual = document.querySelector(`[data-id="${idElemento}"]`)
+    if(elementoAtual){
+        let nome = elementoAtual.querySelector("h4").innerText
+        let preco = elementoAtual.querySelector("p").innerText
+        preco = preco.slice(2)
+        preco = preco.slice(0, -2)
+
+
+        console.log(preco, nome)
+    }
+}
+document.querySelector("#btn-carrinho-nav").addEventListener("click", ()=>{
+  abrirCarrinho()
+})
+document.querySelector("#btn-fechar-carrinho").addEventListener("click", ()=>{
+  fecharCarrinho()
+})
+function abrirCarrinho(){
+  let carrinhoLateral = document.querySelector("#carrinho-lateral");
+  carrinhoLateral.classList.add("carrinho-aberto");
+  document.querySelector("#overlay-carrinho").classList.add("aberto");
+}
+function fecharCarrinho(){
+  let carrinhoLateral = document.querySelector("#carrinho-lateral");
+  carrinhoLateral.classList.remove("carrinho-aberto");
+  document.querySelector("#overlay-carrinho").classList.remove("aberto");
+}
